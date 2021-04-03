ec813391bb97ae87657d9b68d04a9ac6b0af4fcf
yc00534 <yc00534@gmail.com>
2021-04-03 23:20:00 +0200

    addierung klass

---
+++ addierung.py
@@ -0,0 +1,7 @@
+zahl1 = int(input("Bitte geben Sie erste Zahl ein: "))
+zahl2 = int(input("Bitte geben Sie zweite Zahl ein: "))
+zahl3 = int(input("Bitte geben Sie dritte Zahl ein: "))
+
+#addieren und ausgeben
+ergebniss = zahl1 + zahl2 + zahl3
+print("Das Ergebniss der Addition ist ",ergebniss)
