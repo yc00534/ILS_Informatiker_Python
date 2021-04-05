8f11b6a2b9915bb5ac0c5e7d41f622321f038076
yc00534 <yc00534@gmail.com>
2021-04-05 23:27:51 +0200

    ILS: Kryptografie. Einsendeaufgabe Nr 1

---
+++ ErweiterteCaesarVerschluesselung.py
@@ -0,0 +1,16 @@
+#die erweiterte Funktion zur Cäser Verschlüsselung
+def caesar(kette):
+    print("Caesar Verschlüsseung")
+    verschiebung = int(input("Bitte geben SIe den Verschiebewert ein: "))
+
+    #jedes Zeichen um den angegeben Wert und deren Stelle verschieben
+    for zeichen in range(len(kette)):
+
+        #ist es ein großer Buchstabe?
+        if kette[zeichen].isupper():
+            print(chr((ord(kette[zeichen]) + verschiebung + zeichen - 65) % 26 + 65), end="")
+
+        else:
+            print(chr((ord(kette[zeichen]) + verschiebung + zeichen - 97) % 26 + 97), end="")
+
+    print("\n")
