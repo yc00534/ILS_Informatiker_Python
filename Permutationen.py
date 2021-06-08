d5adb1fdb39011a412877366498dc6155aaa462c
yc00534 <yc00534@gmail.com>
2021-06-08 21:51:10 +0200

    ILS: Kryptografie. Einsendeaufgabe Nr 4

---
+++ Permutationen.py
@@ -0,0 +1,22 @@
+#Permutationen für eine Liste
+
+from random import shuffle
+
+def mischen(karten: list, wiederholungen: int):
+    noch_mal = 1
+    while noch_mal <= wiederholungen:
+        shuffle(karten)
+        print("Durchgang: ", noch_mal)
+        print(karten)
+        noch_mal = noch_mal + 1
+
+spielkarten = []
+
+index = 1
+while index <= 32:
+    spielkarten.append(index)
+    index = index + 1
+
+print(spielkarten)
+print("TYP:", type(spielkarten))
+mischen(spielkarten, wiederholungen=59)
