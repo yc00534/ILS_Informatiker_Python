cc6b4d7632ca0970c806c22fabddca091edd51c0
yc00534 <yc00534@gmail.com>
2021-06-08 22:32:44 +0200

    ILS: Sortierung Maximimverfahren

---
+++ Sortierverfahren_Maximum_Auswahl.py
@@ -0,0 +1,44 @@
+from random import randrange
+#die maximale Anzahl der Werte
+anzahl = 2
+
+#eine leere Liste fuer die Werte
+werte = []
+
+print("Einfache Maximumauswahl")
+#die Liste füllen, benutzt werden zufällige Zahlen bis 200
+durchlauf = 1
+while durchlauf <= anzahl:
+    werte.append(randrange(1, 201, 1))
+    durchlauf = durchlauf +1
+
+#zur Kontrolle ausgeben
+print("die unsortierte Werte sind: ")
+for wert in werte:
+    print(wert, end=" ")
+
+#solange die unsortierte Bereich merh als ein Element enthält
+k = anzahl - 1
+while k > 0:
+    #das letzte Element wird zum vorläufigen maximum
+    maximum = k
+    #den unsortierten Bereich durchgehen
+    i = 0
+    while i < k:
+        #ist das neue Element das neue Maximum?
+        if werte[maximum]<werte[i]:
+            maximum = i
+        i = i + 1
+
+    #die Werte tauschen
+    if maximum != k:
+        tausch_temp = werte[k]
+        werte[k] = werte[maximum]
+        werte[maximum]=tausch_temp
+    k = k - 1
+
+#die sortierte Ausgabe
+print()
+print("Die sortierte Werte sind: ")
+for wert in werte:
+    print(wert, end=" ")
