0a20a81bf61af0e1d7b7f48281e652a945f45765
yc00534 <yc00534@gmail.com>
2021-07-07 01:50:52 +0200

    KORREKTUR

--- Getränkeautomat/Getraenkeautomat_V6.py
+++ Getränkeautomat/Getraenkeautomat_V6.py
@@ -3,25 +3,51 @@ Der Getränkeautomat Version 6
 mit der Methode __init__() und __del__()
 ''''''''''''''''''''''''''''''''''''''"""
 
-#die Vereinbahrung der Klasse für die Münzeneinheit
+
+class Getraenke:
+    # die Methode init()
+    def __init__(self, t_name, t_preis, t_anzahl):
+        # die Attribute
+        self._getraenke_name_ = t_name
+        self._getraenke_preis_ = t_preis
+        self._getraenke_flaschen_anzahl_ = t_anzahl
+
+    # die Methode liefert den Getränkenamen zurück
+    def get_getraenke_name(self):
+        return self._getraenke_name_
+
+    # die Methode liefert den Getränkepreis zurück
+    def get_getraenke_preis(self):
+        return self._getraenke_preis_
+
+    # die Methode liefert den Anzahl der Flaschen zurück
+    def get_getraenke_anzahl(self):
+        return self._getraenke_flaschen_anzahl_
+
+    def set_anzahl(self, gekaufte_Menge):
+        self._getraenke_flaschen_anzahl_ = self._getraenke_flaschen_anzahl_ - gekaufte_Menge
+
+
+# die Vereinbahrung der Klasse für die Münzeneinheit
 class Muenzeneinheit:
-    #die Methode __init__()
+    # die Methode __init__()
     def __init__(self):
-        #die Attribute
+        # die Attribute
         self.betrag = 0
         self.noch_zu_zahlen = 0
         self.rueckgeld = 0
+        self.ruck_geld = 0
 
-    #die Methode __del__()
+    # die Methode __del__()
     def __del__(self):
         print("Eine Instanz der Klasse Muenzeneinheit wurde gelöscht")
 
-    #die weiter Methoden
-    def muenzen_annehmen(self,wert):
+    # die weiter Methoden
+    def muenzen_annehmen(self, wert):
         self.noch_zu_zahlen = self.noch_zu_zahlen - wert
 
     def rueckgeld_geben(self):
-        #absoluten Beitrag von noch_zu_zahlen als Rückgeld liefern
+        # absoluten Beitrag von noch_zu_zahlen als Rückgeld liefern
         self.rueckgeld = abs(self.noch_zu_zahlen)
         return self.rueckgeld
 
@@ -32,105 +58,108 @@ class Muenzeneinheit:
     def get_noch_zu_zahlen(self):
         return self.noch_zu_zahlen
 
-#die Vereinbahrung der Klasse für den Automaten
+# die Vereinbahrung der Klasse für den Automaten
 class Getraenkeautomat:
-    #übergeben werden die Anzahl und eine Referenz azf die Münzeneinheit
-    def __init__(self, anzahl1, anzahl2, anzahl3, temp_zahlomat):
-        #die Attribute
-        #jetz ist die Muenzeneinheit Teil des Automaten
+    # Getränke werden erzeugt
+    getraenk1 = Getraenke("Limonade", 10, 10)
+    getraenk2 = Getraenke("Wasser", 8, 10)
+    getraenk3 = Getraenke("Bier", 13, 10)
+
+    getraenkeListe = []
+
+    getraenkeListe.append(getraenk1)
+    getraenkeListe.append(getraenk2)
+    getraenkeListe.append(getraenk3)
+
+    # init Methode mit einer Referenz auf die Münzeneinheit
+    def __init__(self, temp_zahlomat):
+        # die Attribute
+        # jetz ist die Muenzeneinheit Teil des Automaten
         self.zahlomat = temp_zahlomat
 
-        #eine leere Liste fpr die Getränkenahmen
-        self.getraenk = []
-
-        #eine leere Liste fpr die Anzahl der Flaschen
-        self.anzahl_flaschen = []
-
-        #die Getränke eintragen
-        self.getraenk.append("Limonade")
-        self.getraenk.append("Wasser")
-        self.getraenk.append("Bier")
-
-        #die Anzahl der Flaschen
-        #die werden jetzt durch die Argmente gesetzt
-        self.anzahl_flaschen.append(anzahl3)
-        self.anzahl_flaschen.append(anzahl2)
-        self.anzahl_flaschen.append(anzahl1)
-
-        #die Kühlung ist aus
+        # die Kühlung ist aus
         self.kuehlung = False
 
     def __del__(self):
         print("Eine Instanz der Klasse Getraenkeautomat wurde gelöscht.")
 
     def getraenke_waehlen(self):
-        #die Auswahl
+        # die Auswahl
         print("Bitte wählen Sie ein Getränkt.")
         print("Es gibt folgende Auswahl:")
         anzeige_auswahl = 1
-        for getraenk in self.getraenk:
-            print(anzeige_auswahl,getraenk)
+
+        for getraenk in self.getraenkeListe:
+            print(anzeige_auswahl, getraenk.get_getraenke_name())
+            anzeige_auswahl = anzeige_auswahl+1
 
         auswahl = int(input("Geben Sie die gewünschte Nummer ein:"))
+        getraenk_auswahl = self.getraenkeListe[auswahl - 1]
 
-        #gibt es Flaschen von dem Gewaeltem Getränk?
-        if self.anzahl_flaschen[auswahl-1]!=0:
-            #die Anzahl der Flaschen einlesen
+        #Bestandsprüfung/Bestandsermittlung
+        lagerbestand = getraenk_auswahl.get_getraenke_anzahl()
+        # gibt es Flaschen von dem Gewaeltem Getränk?
+
+        if lagerbestand <= 0:
+            print("Das gewählte Getränkt ist leider nicht mehr vorhanden.")
+            auswahl = -1
+            return auswahl
+        else:
+            # die Anzahl der Flaschen einlesen
             anzahl = int(input("Wie viele Flaschen möchten Sie?"))
 
-            #erst muss bezahlt werden
-            #der Preis 10 ist fest vorgeschrieben
-            print("Sie müssen", anzahl*10,"Cent bezahlen.")
-            self.zahlomat.set_beitrag(anzahl*10)
-            while self.zahlomat.get_noch_zu_zahlen()>0:
+            #prüfe ob genugend Flaschen zur Verfügung stehen
+            if anzahl > lagerbestand:
+                anzahl = lagerbestand
+                print("Es sind nur noch", lagerbestand, "Flaschen", getraenk_auswahl.get_getraenke_name(), "vorhanden")
+                print("Sie erhalten den Rest")
+
+            #Zahlvorgang
+            print("Sie müssen", anzahl * getraenk_auswahl.get_getraenke_preis(), "Cent bezahlen.")
+            self.zahlomat.set_beitrag(anzahl * getraenk_auswahl.get_getraenke_preis())
+
+            while self.zahlomat.get_noch_zu_zahlen() > 0:
                 print("Es fehlen noch", self.zahlomat.get_noch_zu_zahlen(), "Cent.")
                 self.zahlomat.muenzen_annehmen(3)
 
-            #das Getraenk ausgeben
-            auswahl = auswahl - 1
-            self.getraenk_ausgeben(anzahl,auswahl)
-        else:
-            print("Das gewählte Getränkt ist leider nicht mehr vorhanden.")
-            auswahl = -1
+            # Ausgabe des Getränkes und evtl Rückgabe des Geldes
+            if self.zahlomat.get_noch_zu_zahlen() < 0:
+                print("Sie erhalten", abs(self.zahlomat.get_noch_zu_zahlen()), "Cent als Rückgeld zurück")
 
-        return auswahl
-    def getraenk_ausgeben(self, anzahl, getraenke_index):
-        #gib es noch genügend Flaschen?
-        if anzahl<=self.anzahl_flaschen[getraenke_index]:
-            print("Sie erhalten,",anzahl, "Flaschen(n)",self.getraenk[getraenke_index])
-            self.anzahl_flaschen[getraenke_index]=self.anzahl_flaschen[getraenke_index]-anzahl
-        else:
-            print("Es sind nur noch", self.anzahl_flaschen[getraenke_index],"Flaschen", self.getraenk[getraenke_index], "vorhanden")
-            print("Sie erhalten den Rest")
-            self.anzahl_flaschen[getraenke_index] = 0
-            #Geld zurück geben
-            print("Sie erhalten", self.zahlomat.rueckgeld_geben(),"Cent zurück.")
+            if self.zahlomat.get_noch_zu_zahlen()<= 0:
+                self.getraenk_ausgeben(anzahl, getraenk_auswahl)
+
+    def getraenk_ausgeben(self, anzahl, getraenk_auswahl):
+        getraenk_auswahl.set_anzahl(anzahl)
+        print("Sie erhalten",anzahl, getraenk_auswahl.get_getraenke_name(), "Flasche/n.")
 
     def kuehlen(self, an_aus):
         self.kuehlung = an_aus
-        if self.kuehlung ==True:
+        if self.kuehlung == True:
             print("Kühlung ist eingeschaltet.")
         else:
             print("Kühlung ist ausgeschaltet.")
 
-#eine Münzeneinheit erzeugen
+
+# eine Münzeneinheit erzeugen
 einheit = Muenzeneinheit()
-#einen Automaten erzeugen
-#die Münzen und die Anzhal der Getränke werden übergeben
-automat = Getraenkeautomat(10,20,30,einheit)
+# einen Automaten erzeugen
+# die Münzen und die Anzhal der Getränke werden übergeben
+automat = Getraenkeautomat(einheit)
 
 auswahl = -1
-#die Kühlung einschalten
+# die Kühlung einschalten
 automat.kuehlen(True)
-#ein Getränk auswählen
+# ein Getränk auswählen
 while auswahl == -1:
     auswahl = automat.getraenke_waehlen()
 
-#die Kühlung einschalten
+# die Kühlung einschalten
 automat.kuehlen(False)
 
-#die Instanzen ausdrücklich freigeben
-#zuerst der Automat
+# die Instanzen ausdrücklich freigeben
+# zuerst der Automat
 del automat
-#und die Münzeneinheit
+# und die Münzeneinheit
 del einheit
+
